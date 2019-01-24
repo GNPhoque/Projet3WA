55b35ff657e35dd210db64635b36fcd7a15cf177
JLafont <lafont_jordan@hotmail.fr>
2019-01-24 16:48:27 +0100

    added actionbar + logout option

--- app/src/main/java/com/example/projet3wa/MainPageActivity.java
+++ app/src/main/java/com/example/projet3wa/MainPageActivity.java
@@ -1,10 +1,13 @@
 package com.example.projet3wa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainPageActivity extends AppCompatActivity {
@@ -15,6 +18,27 @@ public class MainPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout:
+                //Clear shared preferences
+                Intent intent = new Intent(MainPageActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onClickLogout(View v){
     }
 
     @Override
